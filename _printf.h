84e4425bdd8afa3a81f89718bff2fabd4bbf1895
Daniel Kamel <daniel.e.kamel@gmail.com>
2023-11-11 19:58:33 +0200

    Add _printf.h

---
+++ _printf.h
@@ -0,0 +1,26 @@
+#ifndef MAIN
+
+
+#define MAIN
+
+/*Helper files to include*/
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * struct specifier - helps handles sepcifiers in _printf
+ * @c: specifier character provided in format string
+ * @func: function corresponding to specifier
+ * Description: determines the function that adds the
+ * corresponding argument in _printf function to the
+ * buffer string
+*/
+typedef struct specifier
+{
+    char c;
+    void (*func)(va_list ap, char *buffer);
+};
+
+
+#endif
